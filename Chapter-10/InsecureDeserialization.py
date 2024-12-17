7385713e13a7b447b4b7053d1690621b3d72106b
RedSecLabs <samaak@redseclabs.com>
2024-12-17 23:42:08 +0500

    Create Chapter-10

---
+++ Chapter-10/InsecureDeserialization.py
@@ -0,0 +1,8 @@
+# page # 390
+import pickle
+employee_data = {
+    . . . 'employee_id': 1337,
+    . . . 'employee_name': 'TMGM',
+    . . . 'Department': 'IT'
+. . . }
+employee_data{'employee_id': 1337, 'employee_name': 'TMGM', 'Depart-ment': 'IT'}
